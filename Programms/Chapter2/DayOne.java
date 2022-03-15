228af2762ef1b6dc737b6ab319d1d23d1b55546e
Kripton0810 <subhankar0810@gmail.com>
2022-03-15 20:09:29 +0530

    init commit

---
+++ Programms/Chapter2/DayOne.java
@@ -0,0 +1,42 @@
+package Programms.Chapter2;
+
+/**
+ * DayOne
+ */
+public class DayOne {
+    int roll,clas;
+    String name;
+
+    public static void main(String[] args) {
+        // name("anuska");
+        // name("ram");
+        // name("gita");
+        // System.out.println("Hello world");
+        // for (int i = 0; i <= 10; i++) {
+        //     System.out.println("name");
+        // }
+        // name("sita");
+        // name2();
+        DayOne obj = new DayOne();
+        obj.roll = 10;
+        obj.name2();
+        name("rita");
+        
+    }
+    private static void name(String name) {
+        // name2();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(name);
+        }
+    }
+    private  void nms()
+    {
+        roll = 10;
+        name2();
+    }
+    private  void name2() {
+        // name("kolpa");
+        System.out.println("Hello world 2");
+
+    }
+}
